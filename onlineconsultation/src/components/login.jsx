c6e81b49a547e95bab938b572f30b366b6d1a850
iamakashrout <iamakashrout@gmail.com>
2023-06-05 11:49:37 +0530

    Implemented Google Login

--- onlineconsultation/src/components/login.jsx
+++ onlineconsultation/src/components/login.jsx
@@ -1,29 +1,53 @@
 import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Patient_Login from "@/pages/Patient_Login";
 // import videoBg from 'video.mp4'
 const login = () => {
-  return (
-    <>
-      <div className="flex h-screen w-full bg-gray-50">
-        <div className="flex w-full h-screen items-center justify-center">
-          <div className=" bg-white px-10 py-20 rounded-3xl border-2 border-grey-200">
-            <div className="flex justify-center items-center">
-              <h1 className="text-4xl font-semibold text-slate-800">
-                MediConnect Online
-              </h1>
-            </div>
-            <div className="flex flex-col gap-4">
-            <div className="m-10 mt-14 text-lg hover:scale-[1.02] duration-500">
-              <a href="" target="_blank" className="bg-sky-50 text-black py-4 px-20 rounded-md">Login as Patient</a>
-            </div>
-            <div className="mx-10 text-lg hover:scale-[1.02] duration-500">
-              <a href="" target="_blank" className="bg-sky-50 text-black py-4 px-20 rounded-md">Login as Doctor</a>
-            </div>
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        <Patient_Login />
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div className="flex h-screen w-full bg-gray-50">
+          <div className="flex w-full h-screen items-center justify-center">
+            <div className=" bg-white px-10 py-20 rounded-3xl border-2 border-grey-200">
+              <div className="flex justify-center items-center">
+                <h1 className="text-4xl font-semibold text-slate-800">
+                  MediConnect Online
+                </h1>
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="m-10 mt-14 text-lg hover:scale-[1.02] duration-500">
+                  <button
+                    onClick={() => signIn()}
+                    className="bg-sky-50 text-black py-4 px-20 rounded-md"
+                  >
+                    Login as Patient
+                  </button>
+                </div>
+                <div className="mx-10 text-lg hover:scale-[1.02] duration-500">
+                  <button
+                    onClick={() => signIn()}
+                    className="bg-sky-50 text-black py-4 px-20 rounded-md"
+                  >
+                    Login as Doctor
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
+
+  
 };
 
 export default login;
--- onlineconsultation/src/pages/Patient_Login.tsx
+++ onlineconsultation/src/pages/Patient_Login.tsx
@@ -3,46 +3,62 @@ import { FaUserAlt } from "react-icons/fa";
 import Service1 from '../components/Patient_Service1'
 import Service2 from '../components/Patient_Service2'
 import Service3 from '../components/Patient_Service3'
+import { signOut, useSession } from "next-auth/react";
+import Login from '../components/login'
+
+
 const Patient_Login = () => {
-  return (
-    <>
-      <div className="bg-slate-50 w-full h-screen">
-        <div className="w-full px-2 bg-[#8271E4] flex justify-between items-center">
-          <div className="flex justify-between">
-            <div className="text-white">
-              <img
-                src="user3.png"
-                alt=""
-                style={{ width: "80px", height: "80px" }}
-              />
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <div className="bg-slate-50 w-full h-screen">
+          <div className="w-full px-2 bg-[#8271E4] flex justify-between items-center">
+            <div className="flex justify-between">
+              <div className="text-white">
+                <img
+                  src={session.user.image}
+                  alt=""
+                  style={{ width: "80px", height: "80px" }}
+                />
+              </div>
+              <div className="text-white uppercase p-5 text-lg font-medium mt-2">
+                <a href="">MY PROFILE: {session.user.email}</a>
+              </div>
             </div>
-            <div className="text-white uppercase p-5 text-lg font-medium mt-2">
-              <a href="">MY PROFILE</a>
+            <div className="flex justify-center">
+              <ul className="flex justify-between space-x-8 text-white uppercase p-5 mr-12">
+                <li className="text-lg font-medium ml-10">
+                  <a href="#">Home</a>
+                </li>
+                <li className="text-lg font-medium ml-10">
+                  <a href="#">About</a>
+                </li>
+                <li className="text-lg font-medium ml-10">
+                  <a href="#">services</a>
+                </li>
+                <li className="text-lg font-medium ml-10">
+                  <a href="#">Contact Us</a>
+                </li>
+                <li className="text-lg font-medium ml-10">
+                  <button onClick={()=>signOut()}>Sign Out</button>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="flex justify-center">
-            <ul className="flex justify-between space-x-8 text-white uppercase p-5 mr-12">
-              <li className="text-lg font-medium ml-10">
-                <a href="#">Home</a>
-              </li>
-              <li className="text-lg font-medium ml-10">
-                <a href="#">About</a>
-              </li>
-              <li className="text-lg font-medium ml-10">
-                <a href="#">services</a>
-              </li>
-              <li className="text-lg font-medium ml-10">
-                <a href="#">Contact Us</a>
-              </li>
-            </ul>
-          </div>
+          <Service1 />
+          <Service2 />
+          <Service3 />
         </div>
-        <Service1 />
-        <Service2 />
-        <Service3 />
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <Login />
       </div>
-    </>
-  );
+    )
+  }
 };
 
 export default Patient_Login;
