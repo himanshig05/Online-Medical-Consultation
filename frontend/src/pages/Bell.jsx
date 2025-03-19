20852e066b85bc423d60f11408db3e3724821183
Ananyaa <ananyaapriyadarshini0808@gmail.com>
2025-03-19 16:35:05 +0530

    changed local host to base url

--- frontend/src/pages/Bell.jsx
+++ frontend/src/pages/Bell.jsx
@@ -11,7 +11,8 @@ const Bell = ({ doctorEmail }) => {
 
         const fetchPendingRequests = async () => {
             try {
-                const response = await fetch(`http://localhost:5000/api/requests/pending/count/${doctorEmail}`);
+                const response = await fetch(`${BASE_URL}/api/requests/pending/count/${doctorEmail}`);
+
                 if (!response.ok) {
                     throw new Error("Failed to fetch pending requests");
                 }
